44f80e40130ad83b7547a70694125d14f83360a9
PixelsDE <daniel.schoppe2000@gmail.com>
2021-11-08 11:44:14 +0100

    added the is a move legal system (complex)

--- src/main/java/game/map/Field.java
+++ src/main/java/game/map/Field.java
@@ -4,9 +4,8 @@ public class Field {
   private final int[] id;
   private boolean occupied;
   private boolean hole;
-<<<<<<< src/main/java/game/map/Field.java
   private boolean doubleMove = false;
-=======
+
 
   public boolean isDoubleMove() {
     return doubleMove;
@@ -31,8 +30,6 @@ public class Field {
   public boolean isHole() {
     return hole;
   }
-
-  public boolean isRepeat() { return repeat; }
 
   public void setOccupied(boolean occupied) {
     this.occupied = occupied;
