8432878643c8bec4d8b9f9236368e9a9d42ffe6e
PixelsDE <daniel.schoppe2000@gmail.com>
2021-11-15 10:26:27 +0100

    added copyright to all files

---
+++ copyright.md
@@ -0,0 +1,10 @@
+Copyright Notice for $project.name                                            
+Copyright (c)$originalComment.match("Copyright \(c\) (\d+)", 1, "-") at ThunderGames | SwtPra 10 $today.year                              
+File created on $today by $username
+Latest changes made by $username on $file.lastModified
+All contents of "$file.className" are protected by copyright.
+The copyright law, unless expressly indicated otherwise, is  
+at ThunderGames | SwtPra 10. All rights reserved                         
+Any type of duplication, distribution, rental, sale, award,  
+Public accessibility or other use                            
+requires the express written consent of ThunderGames | SwtPra 10.
--- src/main/java/de/thundergames/networking/client/ClientThread.java
+++ src/main/java/de/thundergames/networking/client/ClientThread.java
@@ -1,14 +1,14 @@
 /*
- * Copyright Notice                                             *
- * Copyright (c) ThunderGames 2021                              *
- * Created: 05.05.2018 / 11:59                                  *
- * All contents of this source text are protected by copyright. *
- * The copyright law, unless expressly indicated otherwise, is  *
- * at SwtPra10 | ThunderGames. All rights reserved              *
- * Any type of duplication, distribution, rental, sale, award,  *
- * Public accessibility or other use                            *
- * Requires the express written consent of ThunderGames.        *
- *
+ * Copyright Notice for Swtpra10
+ * Copyright (c) at ThunderGames | SwtPra 10 2021
+ * File created on 15.11.21, 10:24 by Carina
+ * Latest changes made by Carina on 15.11.21, 10:22
+ * All contents of "ClientThread" are protected by copyright.
+ * The copyright law, unless expressly indicated otherwise, is
+ * at ThunderGames | SwtPra 10. All rights reserved
+ * Any type of duplication, distribution, rental, sale, award,
+ * Public accessibility or other use
+ * requires the express written consent of ThunderGames | SwtPra 10.
  */
 package de.thundergames.networking.client;
 
