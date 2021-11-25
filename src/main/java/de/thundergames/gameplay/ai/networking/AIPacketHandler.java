c85c40c1789fe5e724fd814d59a43e31b45e6b04
PixelsDE <carina.schoppe2912@gmail.com>
2021-11-25 17:04:10 +0100

    updated some more stuff and sout

--- src/main/java/de/thundergames/gameplay/ai/networking/AIPacketHandler.java
+++ src/main/java/de/thundergames/gameplay/ai/networking/AIPacketHandler.java
@@ -1,7 +1,8 @@
 /*
  * Copyright Notice for Swtpra10
  * Copyright (c) at ThunderGames | SwtPra10 2021
- * File created on 23.11.21, 13:45 by Carina latest changes made by Carina on 23.11.21, 13:45 All contents of "AIPacketHandler" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
+ * File created on 25.11.21, 17:04 by Carina Latest changes made by Carina on 25.11.21, 17:04
+ * All contents of "AIPacketHandler" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
  * at ThunderGames | SwtPra10. All rights reserved
  * Any type of duplication, distribution, rental, sale, award,
  * Public accessibility or other use
@@ -20,21 +21,22 @@ import org.jetbrains.annotations.NotNull;
 
 public class AIPacketHandler extends ClientPacketHandler {
 
-
   /**
-   * @param ai     the instance of the AI
+   * @param ai the instance of the AI
    * @param packet the packet recieved
    * @throws UndefinedError
    * @author Carina
    * @use the logic for the AI to decide what to do depending on the packet recieved
    */
-  public void handlePacket(@NotNull final AI ai, @NotNull final Packet packet) throws UndefinedError {
+  public void handlePacket(@NotNull final AI ai, @NotNull final Packet packet)
+      throws UndefinedError {
     if (packet.getPacketType().equalsIgnoreCase(Packets.WELCOME.getPacketType())) {
       if (!packet.getValues().get("magic").getAsString().equals("mole42")) {
         System.exit(3);
       }
       ai.getClientThread().setID(packet.getValues().get("clientID").getAsInt());
-      ai.setNetworkPlayer(new NetworkPlayer(ai.getName(), ai.getClientThread().getClientThreadID()));
+      ai.setNetworkPlayer(
+          new NetworkPlayer(ai.getName(), ai.getClientThread().getClientThreadID()));
     } else if (packet.getPacketType().equalsIgnoreCase(Packets.WELCOMEGAME.getPacketType())) {
       handleWelcomeGamePacket(ai, packet);
       ai.getAIUtil().createMapFromJson(ai);
@@ -48,32 +50,24 @@ public class AIPacketHandler extends ClientPacketHandler {
       handleMoleMovedPacket(ai, packet);
     } else if (packet.getPacketType().equalsIgnoreCase(Packets.PLAYERSTURN.getPacketType())) {
       handlePlayersTurnPacket(ai, packet);
-      try {
-        Thread.sleep(100);
-        ai.getLogic().handleAction(ai);
-        if (isTimerRunning()) {
-          timer.purge();
-          timer.cancel();
-          setTimerRunning(false);
-        }
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
+      timerRelatedController(ai);
     } else if (packet.getPacketType().equalsIgnoreCase(Packets.PLAYERPLACESMOLE.getPacketType())) {
       handlePlayerPlacesMolePacket(ai, packet);
-      try {
-        Thread.sleep(100);
-        ai.getLogic().handleAction(ai);
-        if (isTimerRunning()) {
-          timer.purge();
-          timer.cancel();
-          setTimerRunning(false);
-        }
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
+      timerRelatedController(ai);
     }
   }
 
-
+  private void timerRelatedController(@NotNull final AI ai) {
+    try {
+      Thread.sleep(100);
+      ai.getLogic().handleAction(ai);
+      if (isTimerRunning()) {
+        timer.purge();
+        timer.cancel();
+        setTimerRunning(false);
+      }
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
 }
