244d0c2f5c887ca5c305b9ab70830f4805f7d1fc
PixelsDE <daniel.schoppe2000@gmail.com>
2021-12-02 15:34:08 +0100

    changes to players

--- src/main/java/de/thundergames/gameplay/player/ui/LoginScreen.java
+++ src/main/java/de/thundergames/gameplay/player/ui/LoginScreen.java
@@ -9,9 +9,6 @@
  */
 package de.thundergames.gameplay.player.ui;
 
-import java.io.File;
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -21,22 +18,19 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class LoginScreen extends Application {
 
   private final boolean loggedIn = false;
-  @FXML
-  private ResourceBundle resources;
-  @FXML
-  private URL location;
-  @FXML
-  private TextField ip;
-  @FXML
-  private Button login;
-  @FXML
-  private TextField name;
-  @FXML
-  private TextField port;
+  @FXML private ResourceBundle resources;
+  @FXML private URL location;
+  @FXML private TextField ip;
+  @FXML private Button login;
+  @FXML private TextField name;
+  @FXML private TextField port;
 
   /**
    * @param event
@@ -45,25 +39,6 @@ public class LoginScreen extends Application {
    */
   @FXML
   void onLoginButtonClick(ActionEvent event) {
-/*  TODO: Hier
-    String ip = this.ip.getText();
-    String port = this.port.getText();
-    String name = this.name.getText();
-    if (ip != "" && port != "" && name != "" && !loggedIn) {
-      System.out.println("IP: " + ip + " Port: " + port + " Name: " + name);
-      Stage stage = (Stage) login.getScene().getWindow();
-      Client client = new Client(Integer.parseInt(port), ip, name);
-      JSONObject object;
-      object = new JSONObject();
-      object.put("type", Packets.LOGIN.getPacketType());
-      var json = new JSONObject();
-      json.put("name", name);
-      object.put("value", json.toString());
-      client.create();
-      loggedIn = true;
-      client.getClientThread().sendPacket(new Packet(object));
-      stage.close();
-    }*/
   }
 
   @FXML
@@ -83,11 +58,13 @@ public class LoginScreen extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-    location =
+    var loader =
+        new FXMLLoader(
             new File("src/main/java/de/thundergames/gameplay/player/ui/LoginScreen.fxml")
-            .toURI()
-            .toURL();
-    Parent root = FXMLLoader.load(location);
+                .toURI()
+                .toURL());
+    loader.setController(this);
+    Parent root = loader.load();
     primaryStage.setResizable(false);
     primaryStage.setTitle("LoginScreen");
     primaryStage.setScene(new Scene(root));
