defb09a5a040b608fa078917a3e643d8e6d019c5
Issam Mani <issamouu69@gmail.com>
2021-12-10 07:56:31 +0100

    refactor: add constructors and getters to Mole class

--- src/main/java/de/thundergames/playmechanics/Board/Mole.java
+++ src/main/java/de/thundergames/playmechanics/Board/Mole.java
@@ -1,74 +1,68 @@
 package de.thundergames.playmechanics.Board;
-
-import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
-import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 
-import java.awt.*;
+
+
 
 public class Mole extends Button {
-    private final static int DEFAULT_SIZE = 32;
-    private Color color;
-    private Boolean isActive;
-    private double x;
-    private double y;
+    private final static int DEFAULT_SIZE = 48;
+    private int id;
     private int size;
 
-    public Mole(Color color, Boolean isActive, double x, double y, int size) {
-        this.color = color;
-        this.isActive = isActive;
-        this.x = x;
-        this.y = y;
+    public Mole(int id, double x, double y, int size, Boolean isActive ) {
+        super();
+        this.id = id;
         this.size = size;
-        this.setOnAction(e -> this.clickHanlder(e));
     }
 
-    public Mole(double x, double y) {
-        this(Color.BLACK, false, x, y, DEFAULT_SIZE);
+    public Mole(int id, double x, double y, Boolean isActive ) { this(id,x,y, DEFAULT_SIZE, isActive);}
+
+    public Mole(int id, double x, double y) {
+        this(id, x,y , DEFAULT_SIZE, false);
+    }
+
+    public Mole(int id, int size) {
+        this(id, 0,0 , size, false);
+    }
+
+    public Mole(int id) {
+        this(id, 0,0 , DEFAULT_SIZE, false);
+    }
+
+    public int getMoleId() {
+        return this.id;
+    }
+
+    public int getSize( ) {
+        return this.size;
+    }
+
+    public void updatePostion(double x, double y) {
+        this.setLayoutX(x);
+        this.setLayoutY(y);
+        this.render();
     }
 
     private void addStyles() {
         // Set size
-        this.setMaxSize(this.size + 8, this.size + 8);
+        this.setMinSize(this.size , this.size);
         // Add sprite as a background
-        BackgroundImage backgroundImage = new BackgroundImage( new Image( Utils.getSprite("/mole/mole.png"), this.size, this.size, false, true),
+        BackgroundImage backgroundImage = new BackgroundImage( new Image( Utils.getSprite("mole/mole.png"), this.size, this.size, false, true),
                 BackgroundRepeat.NO_REPEAT,
                 BackgroundRepeat.NO_REPEAT,
                 BackgroundPosition.CENTER,
                 BackgroundSize.DEFAULT);
         this.setBackground(new Background(backgroundImage));
-
-
-    }
-
-    private void clickHanlder(ActionEvent e) {
-        // Add drop shadow if selected
-        if(this.isActive) {
-            DropShadow ds = new DropShadow( 20, Color.BLACK );
-            this.setEffect(ds);
-        } else {
-            this.setEffect(null);
-        }
-        this.render();
-        this.isActive = !this.isActive;
     }
 
     public void render() {
         this.addStyles();
-        this.setLayoutX(this.x);
-        this.setLayoutY(this.y);
-
-        Rectangle r = new Rectangle();
-        r.setHeight(20);
-        r.setWidth(20);
-        r.setFill(Color.BLACK);
-        r.setLayoutX(20);
-        r.setLayoutY(20);
     }
+
 
 
 }
