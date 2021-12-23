cef37505a708c4c7985a3e5a2c0694b26fcde0ec
PixelsDE <daniel.schoppe2000@gmail.com>
2021-12-23 17:46:35 +0100

    hotfixes

--- src/main/java/de/thundergames/gameplay/ai/AI.java
+++ src/main/java/de/thundergames/gameplay/ai/AI.java
@@ -1,7 +1,7 @@
 /*
  * Copyright Notice for SwtPra10
  * Copyright (c) at ThunderGames | SwtPra10 2021
- * File created on 23.12.21, 17:16 by Carina Latest changes made by Carina on 23.12.21, 17:10 All contents of "AI" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
+ * File created on 23.12.21, 17:46 by Carina Latest changes made by Carina on 23.12.21, 17:41 All contents of "AI" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
  * at ThunderGames | SwtPra10. All rights reserved
  * Any type of duplication, distribution, rental, sale, award,
  * Public accessibility or other use
@@ -28,7 +28,7 @@ public class AI extends Client {
   private int card;
   private boolean placedMoles = false;
   private int placedMolesAmount = 0;
-  private double sleepingTime = .2;
+  private double sleepingTime = 0;
 
   /**
    * @param ip
