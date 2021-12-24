4058238f7f7077dec757c27a9f90970b2b60dfd6
PixelsDE <carina.schoppe2912@gmail.com>
2021-12-24 10:56:39 +0100

    hotfix

--- src/main/java/de/thundergames/playmechanics/board/GameHandler.java
+++ src/main/java/de/thundergames/playmechanics/board/GameHandler.java
@@ -1,7 +1,8 @@
 /*
  * Copyright Notice for SwtPra10
  * Copyright (c) at ThunderGames | SwtPra10 2021
- * File created on 21.12.21, 16:39 by Carina Latest changes made by Carina on 21.12.21, 16:37 All contents of "GameHandler" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
+ * File created on 24.12.21, 10:56 by Carina Latest changes made by Carina on 23.12.21, 13:20
+ * All contents of "GameHandler" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
  * at ThunderGames | SwtPra10. All rights reserved
  * Any type of duplication, distribution, rental, sale, award,
  * Public accessibility or other use
