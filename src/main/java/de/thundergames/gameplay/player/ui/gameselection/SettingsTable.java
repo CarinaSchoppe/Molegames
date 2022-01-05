5f8323b7ca8c6314447ce94c0b27fea83e5f2574
Philipp Fromme <pfromme@mail.uni-paderborn.de>
2022-01-05 13:41:58 +0100

    Add more information about the game to lobby screen

---
+++ src/main/java/de/thundergames/gameplay/player/ui/gameselection/SettingsTable.java
@@ -0,0 +1,11 @@
+package de.thundergames.gameplay.player.ui.gameselection;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SettingsTable {
+    private String setting;
+    private String value;
+}
