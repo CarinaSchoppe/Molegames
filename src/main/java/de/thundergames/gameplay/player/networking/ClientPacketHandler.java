59f459782e03dfb060f48013915587687614a5bb
PixelsDE <daniel.schoppe2000@gmail.com>
2022-01-09 19:20:08 +0100

    hotfix

--- src/main/java/de/thundergames/gameplay/player/networking/ClientPacketHandler.java
+++ src/main/java/de/thundergames/gameplay/player/networking/ClientPacketHandler.java
@@ -1,7 +1,7 @@
 /*
  * Copyright Notice for SwtPra10
  * Copyright (c) at ThunderGames | SwtPra10 2022
- * File created on 09.01.22, 19:01 by Carina Latest changes made by Carina on 09.01.22, 19:01 All contents of "ClientPacketHandler" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
+ * File created on 09.01.22, 19:20 by Carina Latest changes made by Carina on 09.01.22, 19:18 All contents of "ClientPacketHandler" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
  * at ThunderGames | SwtPra10. All rights reserved
  * Any type of duplication, distribution, rental, sale, award,
  * Public accessibility or other use
@@ -698,7 +698,9 @@ public class ClientPacketHandler {
           "The player: " + player.getName() + " has joined the Game " + client.getGameID() + ".");
       }
     }
-    client.getGameState().getActivePlayers().add(player);
+    if (client.getGameState() != null) {
+      client.getGameState().getActivePlayers().add(player);
+    }
     updateTableView();
     var lobbyObserverGame = LobbyObserverGame.getObserver();
     if (lobbyObserverGame != null) lobbyObserverGame.showNewPlayer();
