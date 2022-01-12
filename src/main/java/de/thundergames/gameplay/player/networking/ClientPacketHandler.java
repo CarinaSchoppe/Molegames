e2353f6e61f590a66d4537c124f1ca67ca67a380
PixelsDE <daniel.schoppe2000@gmail.com>
2022-01-12 12:29:18 +0100

    hotfix

--- src/main/java/de/thundergames/gameplay/player/networking/ClientPacketHandler.java
+++ src/main/java/de/thundergames/gameplay/player/networking/ClientPacketHandler.java
@@ -1,7 +1,7 @@
 /*
  * Copyright Notice for SwtPra10
  * Copyright (c) at ThunderGames | SwtPra10 2022
- * File created on 11.01.22, 20:27 by Carina Latest changes made by Carina on 11.01.22, 20:27 All contents of "ClientPacketHandler" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
+ * File created on 12.01.22, 12:29 by Carina Latest changes made by Carina on 12.01.22, 12:29 All contents of "ClientPacketHandler" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
  * at ThunderGames | SwtPra10. All rights reserved
  * Any type of duplication, distribution, rental, sale, award,
  * Public accessibility or other use
@@ -353,8 +353,6 @@ public class ClientPacketHandler {
    */
   protected void handleFloor() {
     client.getMoles().clear();
-    //TODO: ?????? (carina) client.setGameState(
-    //  new Gson().fromJson(packet.getValues().get("gameState"), GameState.class));
     if (client.getGameState().getPullDiscs().containsKey(client.getClientThread().getThreadID())) {
       client
         .getPullDiscs()
