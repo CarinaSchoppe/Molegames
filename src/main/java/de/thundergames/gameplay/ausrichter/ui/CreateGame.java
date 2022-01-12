5b533c432024b9cabdd1aaa2fef611c96e0feb52
PixelsDE <daniel.schoppe2000@gmail.com>
2022-01-12 12:08:26 +0100

    hotfix

--- src/main/java/de/thundergames/gameplay/ausrichter/ui/CreateGame.java
+++ src/main/java/de/thundergames/gameplay/ausrichter/ui/CreateGame.java
@@ -1,7 +1,7 @@
 /*
  * Copyright Notice for SwtPra10
  * Copyright (c) at ThunderGames | SwtPra10 2022
- * File created on 12.01.22, 12:05 by Carina Latest changes made by Carina on 12.01.22, 12:05 All contents of "CreateGame" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
+ * File created on 12.01.22, 12:08 by Carina Latest changes made by Carina on 12.01.22, 12:08 All contents of "CreateGame" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
  * at ThunderGames | SwtPra10. All rights reserved
  * Any type of duplication, distribution, rental, sale, award,
  * Public accessibility or other use
@@ -420,7 +420,7 @@ public class CreateGame extends Application implements Initializable {
     }
     if (MoleGames.getMoleGames().getServer().isDebug()) {
       System.out.println(
-        "Testing the configuration with a radius of: "
+        "Server: Testing the configuration with a radius of: "
           + radius
           + " and a mole amount of: "
           + numberOfMoles);
