b4a048ec0e48fa7acf6d02fa9f2b2592713b8f33
Carina Schoppe <carina.schoppe2912@gmail.com>
2022-01-13 22:19:03 +0100

    hotfix

--- src/main/java/de/thundergames/playmechanics/game/GameUtil.java
+++ src/main/java/de/thundergames/playmechanics/game/GameUtil.java
@@ -1,7 +1,7 @@
 /*
  * Copyright Notice for SwtPra10
  * Copyright (c) at ThunderGames | SwtPra10 2022
- * File created on 13.01.22, 22:17 by Carina Latest changes made by Carina on 13.01.22, 22:17 All contents of "GameUtil" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
+ * File created on 13.01.22, 22:19 by Carina Latest changes made by Carina on 13.01.22, 22:18 All contents of "GameUtil" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
  * at ThunderGames | SwtPra10. All rights reserved
  * Any type of duplication, distribution, rental, sale, award,
  * Public accessibility or other use
@@ -114,8 +114,7 @@ public class GameUtil {
       }
       nextFloor();
       return;
-    }
-    if (allPlayerMolesInHoles(game.getCurrentPlayer())) {
+    } else if (allPlayerMolesInHoles(game.getCurrentPlayer())) {
       if (MoleGames.getMoleGames().getServer().isDebug()) {
         System.out.println(
           "Server: All player moles are in holes! for player: " + game.getCurrentPlayer().getName());
@@ -133,8 +132,7 @@ public class GameUtil {
       } else {
         nextPlayer();
       }
-    }
-    if (allMolesOfAllPlayersInHoles()) {
+    } else if (allMolesOfAllPlayersInHoles()) {
       if (MoleGames.getMoleGames().getServer().isDebug()) {
         System.out.println("Server: All moles of all players are in holes!");
       }
