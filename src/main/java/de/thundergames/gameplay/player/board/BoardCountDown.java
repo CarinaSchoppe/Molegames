f64a3969f9d711bfa3233fe5aa8c053b24395a5c
Carina Schoppe <carina.schoppe2912@gmail.com>
2022-01-13 17:45:06 +0100

    cleanup hotfix

--- src/main/java/de/thundergames/gameplay/player/board/BoardCountDown.java
+++ src/main/java/de/thundergames/gameplay/player/board/BoardCountDown.java
@@ -1,7 +1,7 @@
 /*
  * Copyright Notice for SwtPra10
  * Copyright (c) at ThunderGames | SwtPra10 2022
- * File created on 10.01.22, 22:08 by Carina Latest changes made by Carina on 10.01.22, 22:08 All contents of "BoardCountDown" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
+ * File created on 13.01.22, 17:45 by Carina Latest changes made by Carina on 13.01.22, 17:45 All contents of "BoardCountDown" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
  * at ThunderGames | SwtPra10. All rights reserved
  * Any type of duplication, distribution, rental, sale, award,
  * Public accessibility or other use
@@ -16,8 +16,8 @@ import java.util.TimerTask;
 public class BoardCountDown {
 
   private long remainingTime;
-  private Boolean showCount;
-  private Boolean stopCountAfterTurn;
+  private boolean showCount;
+  private boolean stopCountAfterTurn;
 
   public void setRemainingTime(long remainingTime) {
     this.remainingTime = remainingTime;
@@ -26,8 +26,8 @@ public class BoardCountDown {
 
   public void setTimer(boolean run) {
     this.showCount = run;
-    Timer timer = new Timer();
-    TimerTask task = new TimerTask() {
+    var timer = new Timer();
+    var task = new TimerTask() {
       @Override
       public void run() {
         if (!showCount) {
