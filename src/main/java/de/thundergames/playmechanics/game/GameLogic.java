13a44ad20aa1a0e5964d9bef52bd06d0d708a98d
Carina Schoppe <carina.schoppe2912@gmail.com>
2022-01-13 17:14:51 +0100

    cleanup hotfix

--- src/main/java/de/thundergames/playmechanics/game/GameLogic.java
+++ src/main/java/de/thundergames/playmechanics/game/GameLogic.java
@@ -1,7 +1,7 @@
 /*
  * Copyright Notice for SwtPra10
  * Copyright (c) at ThunderGames | SwtPra10 2022
- * File created on 11.01.22, 20:01 by Carina Latest changes made by Carina on 11.01.22, 19:42 All contents of "GameLogic" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
+ * File created on 13.01.22, 17:14 by Carina Latest changes made by Carina on 13.01.22, 17:09 All contents of "GameLogic" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
  * at ThunderGames | SwtPra10. All rights reserved
  * Any type of duplication, distribution, rental, sale, award,
  * Public accessibility or other use
@@ -198,7 +198,6 @@ public class GameLogic {
    * @use moves a mole depending on in a hole or on a field
    */
   public synchronized boolean isPlayerMovePossible(@NotNull final Player player) {
-    var possible = false;
     for (var mole : player.getMoles()) {
       if (mole == null) {
         continue;
