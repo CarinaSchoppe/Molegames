ea4379072f40e0da6cb647f80fdc5c3fba698826
Carina Schoppe <carina.schoppe2912@gmail.com>
2022-01-17 22:27:47 +0100

    hotfix

--- src/main/java/de/thundergames/gameplay/ausrichter/ui/PlayerManagement.java
+++ src/main/java/de/thundergames/gameplay/ausrichter/ui/PlayerManagement.java
@@ -1,7 +1,7 @@
 /*
  * Copyright Notice for SwtPra10
  * Copyright (c) at ThunderGames | SwtPra10 2022
- * File created on 17.01.22, 22:26 by Carina Latest changes made by Carina on 17.01.22, 22:26 All contents of "PlayerManagement" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
+ * File created on 17.01.22, 22:27 by Carina Latest changes made by Carina on 17.01.22, 22:27 All contents of "PlayerManagement" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
  * at ThunderGames | SwtPra10. All rights reserved
  * Any type of duplication, distribution, rental, sale, award,
  * Public accessibility or other use
@@ -139,6 +139,7 @@ public class PlayerManagement implements Initializable {
       }
       if (selectedPlayer.getGame().getCurrentGameState() == GameStates.OVER) {
         MoleGames.getMoleGames().getServer().getPacketHandler().handlePlayerLeavePacket((ServerThread) selectedPlayer.getServerClient());
+        Dialog.show("Der Spieler wurde aus dem Spiel entfernt!", "Erfolg beim entfernen!", Dialog.DialogType.INFO);
       }
       updateTable();
     } else {
