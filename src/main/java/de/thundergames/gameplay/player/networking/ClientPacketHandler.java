665acf9b858d6e8d6c21d0e6a918b852ae34a7fa
Carina Schoppe <carina.schoppe2912@gmail.com>
2022-01-20 18:32:07 +0100

    hotfix (AI-Anzeige)

--- src/main/java/de/thundergames/gameplay/player/networking/ClientPacketHandler.java
+++ src/main/java/de/thundergames/gameplay/player/networking/ClientPacketHandler.java
@@ -1,7 +1,7 @@
 /*
  * Copyright Notice for SwtPra10
  * Copyright (c) at ThunderGames | SwtPra10 2022
- * File created on 17.01.22, 19:10 by Carina Latest changes made by Carina on 17.01.22, 19:10 All contents of "ClientPacketHandler" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
+ * File created on 20.01.22, 18:32 by Carina Latest changes made by Carina on 20.01.22, 18:30 All contents of "ClientPacketHandler" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
  * at ThunderGames | SwtPra10. All rights reserved
  * Any type of duplication, distribution, rental, sale, award,
  * Public accessibility or other use
@@ -642,7 +642,9 @@ public class ClientPacketHandler {
       }
     }
     try {
-      new Thread(() -> new LeaderBoard().create(score)).start();
+      if (!(client instanceof AI)) {
+        new Thread(() -> new LeaderBoard().create(score)).start();
+      }
     } catch (IllegalStateException ignored) {
     }
     updateTableView();
